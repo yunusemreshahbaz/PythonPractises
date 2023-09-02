7ee987959ab12a755dcf2283e2740a99391d569b
Yunus <114196646+Yuaz3578-51@users.noreply.github.com>
2023-09-02 15:45:19 +0300

    basic ui practises

---
+++ gün10/anaekran.py
@@ -0,0 +1,15 @@
+import sys
+from PyQt5.QtWidgets import *
+from anaekran_ui import Ui_Anaekran
+
+class AnaekranPenceresi(QWidget,Ui_Anaekran):
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    pencere = AnaekranPenceresi()
+    pencere.show()
+    sys.exit(app.exec_())
